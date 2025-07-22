ce005c03aa40acdac72fee750b1712583840ba2f
Milad Nekofar <milad@nekofar.com>
2025-07-23 00:05:01 +0300

    refactor(worker): remove redundant comments
    eliminate introductory comments related to Cloudflare Workers for cleaner and more maintainable code.
    
    these comments were unnecessary and did not add value to the codebase, ensuring a more focused implementation.

--- src/index.ts
+++ src/index.ts
@@ -57,23 +57,6 @@ async function retrieveLilNounsRelatedMessages(
   return messages;
 }
 
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Run `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"` to see your Worker in action
- * - Run `npm run deploy` to publish your Worker
- *
- * Bind resources to your Worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.s/workers/
- */
-
 export default {
   async fetch(req) {
     const url = new URL(req.url);
